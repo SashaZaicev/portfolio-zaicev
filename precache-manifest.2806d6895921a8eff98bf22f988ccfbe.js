9ce2d41bdbd44debbfd01615c4cc2834c7b5ac4a
SashaZaicev <sasha167@bk.ru>
2019-10-17 22:25:36 +0300

    Updates

---
+++ precache-manifest.2806d6895921a8eff98bf22f988ccfbe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7fc0c56f2bd41eb7e84a4748349890",
+    "url": "/portfolio-zaicev/index.html"
+  },
+  {
+    "revision": "90bf946e669760d6984b",
+    "url": "/portfolio-zaicev/static/css/main.96350163.chunk.css"
+  },
+  {
+    "revision": "566457aa7bd9ee695e26",
+    "url": "/portfolio-zaicev/static/js/2.187d1b21.chunk.js"
+  },
+  {
+    "revision": "90bf946e669760d6984b",
+    "url": "/portfolio-zaicev/static/js/main.dbb40dbc.chunk.js"
+  },
+  {
+    "revision": "602e38c5ee946dca1fc2",
+    "url": "/portfolio-zaicev/static/js/runtime~main.4733d57e.js"
+  },
+  {
+    "revision": "db6bc0ad709e12513ebb30460aef00ed",
+    "url": "/portfolio-zaicev/static/media/calculator.db6bc0ad.jpg"
+  },
+  {
+    "revision": "3ebd61f7dcd409da550f42706f2c3b52",
+    "url": "/portfolio-zaicev/static/media/facebook.3ebd61f7.svg"
+  },
+  {
+    "revision": "bdd8b44aa5867d038c1000e19ab74586",
+    "url": "/portfolio-zaicev/static/media/html-icon.bdd8b44a.svg"
+  },
+  {
+    "revision": "46b346c9daabb112e6a651f60cdeba03",
+    "url": "/portfolio-zaicev/static/media/js-icon.46b346c9.svg"
+  },
+  {
+    "revision": "3b70d0fc31c484bc55aaaf84e2262a04",
+    "url": "/portfolio-zaicev/static/media/linkedin.3b70d0fc.svg"
+  },
+  {
+    "revision": "151e1a817730225dd372a121109cf80e",
+    "url": "/portfolio-zaicev/static/media/react-icon.151e1a81.svg"
+  },
+  {
+    "revision": "2955967c57458a2518491a5461804aa0",
+    "url": "/portfolio-zaicev/static/media/social-network.2955967c.jpg"
+  },
+  {
+    "revision": "89a590364b80b7e06be5207f9169cba0",
+    "url": "/portfolio-zaicev/static/media/telegram2.89a59036.svg"
+  },
+  {
+    "revision": "0570caf12588f65bf6e85780ea23a436",
+    "url": "/portfolio-zaicev/static/media/todolist.0570caf1.jpg"
+  },
+  {
+    "revision": "8c9399e31ce755970497937f81573e54",
+    "url": "/portfolio-zaicev/static/media/vk.8c9399e3.svg"
+  }
+]);
