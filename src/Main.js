63e7c20deb7fc35ff5a417d3effe0398c8f73568
Sasha Zaitsau <46328412+SashaZaicev@users.noreply.github.com>
2021-04-01 22:20:08 +0300

    add download CV

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Main.module.css';
+import MyCV from './assets/CV Zaitsau.pdf'
 
 function Main() {
     return (
@@ -10,11 +11,13 @@ function Main() {
                     <span>Hello!</span>
                     <span>My name <span>Alexander</span></span>
                     <h1>I am front-end-developer</h1>
+                    <div className={styles.downloadCV}><span>My CV: <div>View  <a href={MyCV} target='_blank'>
+                        {`<<<`}HERE{`>>>`}</a></div><div>Download  <a href={MyCV} download>
+                        {`<<<`}HERE{`>>>`}</a></div></span></div>
                 </div>
                 <div className={styles.photo}>
-                   <div className={styles.image}>
-
-                   </div>
+                    <div className={styles.image}>
+                    </div>
                 </div>
             </div>
         </div>
